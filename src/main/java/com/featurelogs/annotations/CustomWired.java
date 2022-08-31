bceb9de16bf8686fd066fa6d49e623895d4b0ac1
sachin <verma.sachin@outlook.in>
2022-08-31 17:20:51 +0530

    clean master

--- src/main/java/com/featurelogs/annotations/CustomWired.java
+++
@@ -1,9 +0,0 @@
-package com.featurelogs.annotations;
-
-import java.lang.annotation.*;
-
-@Documented
-@Target({ElementType.TYPE,ElementType.FIELD})
-@Retention(RetentionPolicy.RUNTIME)
-public @interface CustomWired {
-}
